1f21484f04c27ab312e4154d45b491de0aa6642f
James Pike <jpike@chilon.net>
2015-02-16 14:00:16 +0000

    stores/loads switch statuses and can rename switches.

--- bin/catsite.py
+++ bin/catsite.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 from bottle import route, get, run, template, static_file
+import json
+import xdg.BaseDirectory
 
-switches = { '1': 'unknown', '2': 'unknown', '3': 'unknown', '4': 'unknown'}
+# { name: { idx, status } }
+switches = {}
+
+def save_status():
+  path = xdg.BaseDirectory.save_data_path('catsite')
+  with open(path + '/switches', 'w') as outfile:
+    json.dump(switches, outfile)
 
 @get('/status')
 def get_status():
@@ -10,9 +18,11 @@ def get_status():
 def turn(status, id):
   if id is None:
     for id in switches.copy().keys():
-      switches[id] = status
+      switches[id]['status'] = status
   elif id in switches:
-    switches[id] = status
+    switches[id]['status'] = status
+
+  save_status()
   return get_status()
 
 @get('/on')
@@ -25,12 +35,42 @@ def turn_on(id=None):
 def turn_off(id=None):
   return turn('off', id)
 
+@get('/rename/<id>/<new_id>')
+def rename(id, new_id):
+  switch = switches[id]
+  del(switches[id])
+  switches[new_id] = switch
+
+  save_status()
+  return get_status()
+
 @route('/')
 @route('/<filename:path>')
 def serve_static(filename='index.html'):
   return static_file(filename, root='client')
 
 def main():
+  global switches
+
+  # load most specific data path
+  path = None
+  for path in xdg.BaseDirectory.load_data_paths('catsite'):
+    print('loading ' + path)
+    try:
+      with open(path + '/switches', 'r') as infile:
+        switches = json.load(infile)
+    except FileNotFoundError:
+      pass
+
+  if not switches:
+    for idx in range(1, 4):
+      switches[idx] = { 'idx': idx }
+
+  print('switches', switches)
+
+  for switch in switches.values():
+    switch['status'] = 'unknown'
+
   run(host='0.0.0.0', port=8144)
 
 main()
