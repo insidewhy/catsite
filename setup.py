adc8d98851f2fea15c6581c6f63287e3e2464fd0
James Pike <jpike@chilon.net>
2015-02-17 00:46:50 +0000

    setup.py fix version 0.0.2

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='catsite',
-    version='0.0.1',
+    version='0.0.2',
     author='J. Pike',
     author_email='pip@chilon.net',
-    scripts='bin/catsite.py',
+    scripts=['bin/catsite.py'],
     url='http://github.com/nuisanceofcats/catsite',
     license='LICENSE.txt',
     description='Website control for energenie radio control board.',
